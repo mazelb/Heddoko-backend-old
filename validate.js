9d14b1a075d9ece99b33b1a3e2b58700ad7d8e88
Loay Gewily <loay@users.noreply.github.com>
2014-10-24 15:34:11 -0400

    Create validate.js

---
+++ validate.js
@@ -0,0 +1,81 @@
+function validateForm(theForm) {
+	
+var reason = "";
+
+  reason += validateEmail(theForm.email);
+  reason += validatePassword(theForm.password); 
+  reason += validateConfirm(theForm.confirmPassword);
+  
+  if (reason != "") {
+    alert("Some fields need correction:\n" + reason);
+    return false;
+  }
+
+  return true;
+}
+
+
+
+
+function validatePassword(fld) {
+    var error = "";
+    var illegalChars = /[\W_]/; // allow only letters and numbers 
+ 
+    if (fld.value == "") {
+        fld.style.background = 'Red';
+        error = "You didn't enter a password.\n";
+    } else if ((fld.value.length < 7)) {
+        error = "Password must be at least 7 characters  \n";
+        fld.style.background = 'Red';
+    } else if (illegalChars.test(fld.value)) {
+        error = "The password contains illegal characters.\n";
+        fld.style.background = 'Red';
+    } else if (!((fld.value.search(/(a-z)+/)) && (fld.value.search(/(0-9)+/)))) {
+        error = "The password must contain at least one numeral.\n";
+        fld.style.background = 'Red';
+    } else {
+        fld.style.background = 'White';
+    }
+   return error;
+}  
+
+function trim(s)
+{
+  return s.replace(/^\s+|\s+$/, '');
+} 
+
+
+
+function validateEmail(fld) {
+    var error="";
+    var tfld = trim(fld.value);                        // value of field with whitespace trimmed off
+    var emailFilter = /^[^@]+@[^@.]+\.[^@]*\w\w$/ ;
+    var illegalChars= /[\(\)\<\>\,\;\:\\\"\[\]]/ ;
+    
+    if (fld.value == "") {
+        fld.style.background = 'Red';
+        error = "You didn't enter an email address.\n";
+    } else if (!emailFilter.test(tfld)) {              //test email for illegal characters
+        fld.style.background = 'Red';
+        error = "Please enter a valid email address.\n";
+    } else if (fld.value.match(illegalChars)) {
+        fld.style.background = 'Red';
+        error = "The email address contains illegal characters.\n";
+    } else {
+        fld.style.background = 'White';
+    }
+    return error;
+}
+
+
+
+
+
+function validateConfirm(fld){
+	 var error = "";
+		    if( fld.value!=document.register.password.value){
+      error = "Passwords do not match .\n";
+        fld.style.background = 'Red';
+    }
+     return error;
+}

059ea34310c7cd312997f1f763b1d04b00fa98ed
frnkly <frank@frnk.ca>
2015-08-24 16:20:32 -0400

    Updated search

--- Dashboards/Suit Management Dashboard/resources/views/partials/materials-tab.blade.php
+++ Dashboards/Suit Management Dashboard/resources/views/partials/materials-tab.blade.php
@@ -1,36 +1,10 @@
 
 {{-- Search form --}}
-<form ng-submit="data.materials.updatePage()">
-    <div class="input-group">
-        <span class="input-group-addon" id="search-bar-addon">Search</span>
-        <input
-            type="text"
-            class="form-control"
-            placeholder="Enter the name or part # of a material"
-            aria-describedby="search-bar-addon"
-            ng-model="data.materials.search_term">
-
-        <span class="input-group-btn">
-            <button class="btn btn-default" type="button" ng-click="data.materials.updatePage()">Search</button>
-            <button
-                type="button"
-                class="btn btn-default dropdown-toggle"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false">
-                    Show <span>@{{ data.materials.per_page }}</span> materials per page
-                    <span class="caret"></span>
-            </button>
-            <ul class="dropdown-menu dropdown-menu-right">
-                <li><a href="#" ng-click="data.materials.per_page=5">Show 5 materials per page</a></li>
-                <li><a href="#" ng-click="data.materials.per_page=10">Show 10 materials per page</a></li>
-                <li><a href="#" ng-click="data.materials.per_page=20">Show 20 materials per page</a></li>
-                <li><a href="#" ng-click="data.materials.per_page=50">Show 50 materials per page</a></li>
-                <li><a href="#" ng-click="data.materials.per_page=100">Show 100 materials per page</a></li>
-            </ul>
-        </span>
-    </div>
-</form>
+@include('partials/_search-form', [
+    'model_name' => 'materials',
+    'data_object' => 'materials',
+    'placeholder' => 'Enter the name or part # of a material'
+])
 <br />
 
 Total materials matching this query: <span class="badge">@{{data.materials.total}}</span>
@@ -61,11 +35,9 @@ Total materials matching this query: <span class="badge">@{{data.materials.total
                 <input type="text" ng-model="data.materials.new_item.part_no" class="form-control" placeholder="Part #" />
             </div>
             <div class="col-sm-4">
-                <select
-                    ng-model="data.materials.new_item.material_type_id"
-                    ng-selected="data.materials.new_item.material_type_id"
-                    ng-options="material_type.identifier for material_type in data.material_type.list track by material_type.id"
-                    class="form-control">
+                <select ng-model="data.materials.new_item.material_type_id" class="form-control">
+                    <option value="0">-- select a material type --</option>
+                    <option ng-repeat="material_type in data.material_type.list" value="@{{material_type.id}}">@{{material_type.identifier}}</option>
                 </select>
             </div>
         </div>
